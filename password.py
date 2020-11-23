5863f3bc8fcb64328141c18b4adf9cb44cf8de02
peterkush <pkungu004@gmail.com>
2020-11-23 03:59:57 +0300

    defined save and delete user function

--- password.py
+++ password.py
@@ -2,5 +2,9 @@ class User:
     def __init__(self,user_name,User_password):
         self.user_name = user_name
         self.User_password = User_password
+    def save_user(self):
+        User.user_list.append(self)
+    def delete_user(self ):
+        User.user_list.remove(self)
 
     
