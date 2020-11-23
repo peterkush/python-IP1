351a02a297c2a5424b88a4ecc8ae6a09180e3b10
peterkush <pkungu004@gmail.com>
2020-11-23 04:49:42 +0300

    difined various funtion and the main call that allow app to run

--- run.py
+++ run.py
@@ -8,3 +8,21 @@ def create_user(name, user_password):
     return new_user
 def generate_password(user):
     return user.generate_random_password()
+def save_user(user):
+    user.save_user()
+def delete_user(user):
+    user.delete_user()
+def create_credential(account, account_username, account_password):
+    new_credential = Credential(account, account_username, account_password)
+    return new_credential
+def save_credential(credential):
+    credential.save_credential()
+def delete_credential(credential):
+    credential.delete_credential()
+def find_credential(account_username):
+    return Credential.find_by_account_username(account_username)
+def check_existing_credential(account_username):
+    return Credential.find_by_account_username(account_username)
+def display_credential():
+    return Credential.display_all_credential()
+
