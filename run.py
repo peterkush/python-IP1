a0558e8de8c4440a6eede436c66e9e50c234623e
peterkush <pkungu004@gmail.com>
2020-11-23 04:41:38 +0300

    difined two funrion under run.py

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ from password import User, Credential
 import random
 import string
 import getpass
+
+def create_user(name, user_password):
+    new_user = User(name, user_password)
+    return new_user
+def generate_password(user):
+    return user.generate_random_password()
