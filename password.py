4df5842900892248ef90992b7caa9ec03421e984
peterkush <pkungu004@gmail.com>
2020-11-23 04:29:14 +0300

    added the classmethod with all credential

--- password.py
+++ password.py
@@ -20,6 +20,25 @@ class Credential:
     def delete_credential(self):
         Credential.credential_list.remove(self)
 
-    
+
+    @classmethod
+    def find_by_account_username(cls,account_username):
+        for credential in cls.credential_list:
+            if credential.account_username == account_username:
+              return credential
+
+    @classmethod 
+    def credential_exists(cls,account_username):
+       for credential in cls.credential_list:
+           if credential.account_username == account_username:
+               return True
+       return False          
+
+    @classmethod
+    def display_credential(cls):
+        return cls.credential_list
+    @classmethod
+    def display_all_credential(cls):
+       return cls.credential_list
 
     
