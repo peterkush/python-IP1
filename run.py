4abbb967293038a7d08224ee7968a9cbc0e71130
peterkush <pkungu004@gmail.com>
2020-11-23 04:59:56 +0300

    added the function in collecting the data via ternimal

--- run.py
+++ run.py
@@ -31,6 +31,87 @@ def main():
     user_name = input("Enter your name..")
     print(f"Hello {user_name},welcome  to password locker")
     print("\n")
+    ask = input(f"Hello { user_name} .Do you have an account ? YES?NO ..").lower()
+    if ask == "no":
+        print("Sign in to password locker")
+        user_name = input("Enter your name..") 
+        print(f"Hello {user_name} .Generated password ?YES/NO >"
+
+        ) 
+        if create_user == "no":
+            print("_"*167)
+            print("|Passwords may not be visible coz they are secured|")
+            print(""*67)
+            getpass.getpass()
+            print("You are logged in")
+            while True:
+
+                      print("Use these short codes : cc - create a new credential, dc - display credential, fc - find credential, dl - delete credential, gp - generate random password, ex - exit the credential list")
+                      short_code = input().lower()
+                      if short_code == 'cc':
+                           print("create account")
+                           print("_"*10)
+                           
+                           print("Account.....")
+                           account = input(">")
+
+                           print("usename....")
+                           account_username = input(">")
+
+                           print("Enter password")
+                           account_password = input(">")
+
+                           save_credential(create_credential(account, account_username, account_password))
+
+                           print("\n")
+                           print(f"New Credential {account} {account_username} {account_password} has been created")
+                           print("\n")
+        elif short_code == "gp":
+                        print("Please enter the account you want to generate password for > ")
+                        social_media = input("Enter account type > ")
+
+                        def random_password(string_length):
+                            letters = string.ascii_letters
+                            return "".join(random.choice(letters) for i in range(string_length))
+
+                        print(f"Your random password for {social_media} is:", random_password(8))
+
+        elif short_code == "dc":
+                        if display_credential():
+                            print("Here is a list of credentials and password")
+                            print("\n")
+                        else:
+                            print("\n")
+                            print("You do not have any saved credential, try saving one")
+                            print("\n")
+        elif short_code == "fc":
+                        print("Enter the account username you are searching")
+                        search_account_username = input()
+                        if check_existing_credential(search_account_username):
+                            search_credential = find_credential(search_account_username)
+                            print(f"{search_credential.account} {search_credential.account_username}")
+                            print('_'*20)
+                            print(f"Account password...{search_credential.account_password}")
+                        else:
+                            print("The credential does not exist.Try to resign in")
+
+        elif short_code == "dl":
+                        print("Enter the account username your will like to delete.")
+                        my_delete = input(">")
+                        my_del = find_credential(my_delete)
+                        Credential.credential_list.remove(my_del)
+                        print(f"Credential with account username {my_delete} has been removed successfully")
+
+
+        elif short_code == "ex":
+                        print("logged out")
+                         #exit
+
+        else:
+                        print("please check your entry")
+
+
+
 
 
 
